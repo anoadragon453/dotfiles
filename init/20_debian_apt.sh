4aad5721597a9b91ba706a762f497d48f77b4f3f
Andrew Morgan <andrew@amorgan.xyz>
2019-01-31 21:39:06 +0000

    Change packages to my own personal tastes

---
+++ init/20_debian_apt.sh
@@ -0,0 +1,198 @@
+# Debian-only stuff. Abort if not Debian.
+is_debian || return 1
+
+apt_keys=()
+apt_source_files=()
+apt_source_texts=()
+apt_packages=()
+deb_installed=()
+deb_sources=()
+
+installers_path="$DOTFILES/caches/installers"
+
+#############################
+# WHAT DO WE NEED TO INSTALL?
+#############################
+
+# Applications
+apt_packages+=(
+  chromium-browser
+  mpv
+  rofi
+  vlc
+)
+
+# Development
+apt_packages+=(
+  adb
+  build-essential
+  docker.io # Needs to be downloaded in a special way?
+  docker-compose
+  fastboot
+  git
+	git-extras
+  jq
+  neovim
+  python-pip
+  python3-pip
+  zenmap
+)
+
+# Tools
+apt_packages+=(
+  curl
+  htop
+  ncdu
+  nmap
+  peek
+  ripgrep
+  thefuck
+  tmux
+  tree
+)
+
+# Misc.
+apt_packages+=(
+  apt-transport-https
+  grub-customizer
+  imagemagick
+  xclip
+  zsh
+)
+
+## Applications
+
+# http://askubuntu.com/questions/854480/how-to-install-the-steam-client/854481#854481
+apt_packages+=(python-apt)
+deb_installed+=(/usr/bin/steam)
+deb_sources+=(deb_source_steam)
+function deb_source_steam() {
+  local steam_root steam_file
+  steam_root=http://repo.steampowered.com/steam/pool/steam/s/steam/
+  steam_file="$(wget -q -O- "$steam_root?C=M;O=D" | sed -En '/steam-launcher/{s/.*href="([^"]+)".*/\1/;p;q;}')"
+  echo "$steam_root$steam_file"
+}
+
+# https://discordapp.com/download
+# TODO: Get this shit outta here
+deb_installed+=(/usr/bin/discord)
+deb_sources+=("https://discordapp.com/api/download?platform=linux&format=deb")
+
+## Development
+
+# https://code.visualstudio.com/docs/setup/linux
+apt_keys+=(https://packages.microsoft.com/keys/microsoft.asc)
+apt_source_files+=(vscode.list)
+apt_source_texts+=("deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main")
+apt_packages+=(code)
+
+function finally() {
+  # Install misc bins from zip file.
+  #install_from_zip terraform 'https://releases.hashicorp.com/terraform/0.9.2/terraform_0.9.2_linux_amd64.zip'
+}
+
+####################
+# ACTUALLY DO THINGS
+####################
+
+# Add APT keys.
+keys_cache=$DOTFILES/caches/init/apt_keys
+IFS=$'\n' GLOBIGNORE='*' command eval 'setdiff_cur=($(<$keys_cache))'
+setdiff_new=("${apt_keys[@]}"); setdiff; apt_keys=("${setdiff_out[@]}")
+unset setdiff_new setdiff_cur setdiff_out
+
+if (( ${#apt_keys[@]} > 0 )); then
+  e_header "Adding APT keys (${#apt_keys[@]})"
+  for key in "${apt_keys[@]}"; do
+    e_arrow "$key"
+    if [[ "$key" =~ -- ]]; then
+      sudo apt-key adv $key
+    else
+      wget -qO- $key | sudo apt-key add -
+    fi && \
+    echo "$key" >> $keys_cache
+  done
+fi
+
+# Add APT sources.
+function __temp() { [[ ! -e /etc/apt/sources.list.d/$1.list ]]; }
+source_i=($(array_filter_i apt_source_files __temp))
+
+if (( ${#source_i[@]} > 0 )); then
+  e_header "Adding APT sources (${#source_i[@]})"
+  for i in "${source_i[@]}"; do
+    source_file=${apt_source_files[i]}
+    source_text=${apt_source_texts[i]}
+    if [[ "$source_text" =~ ppa: ]]; then
+      e_arrow "$source_text"
+      sudo add-apt-repository -y $source_text
+    else
+      e_arrow "$source_file"
+      sudo sh -c "echo '$source_text' > /etc/apt/sources.list.d/$source_file.list"
+    fi
+  done
+fi
+
+# Update APT.
+e_header "Updating APT"
+sudo apt-get -qq update
+
+# Only do a dist-upgrade on initial install, otherwise do an upgrade.
+e_header "Upgrading APT"
+if is_dotfiles_bin; then
+  sudo apt-get -qy upgrade
+else
+  sudo apt-get -qy dist-upgrade
+fi
+
+# Install APT packages.
+installed_apt_packages="$(dpkg --get-selections | grep -v deinstall | awk 'BEGIN{FS="[\t:]"}{print $1}' | uniq)"
+apt_packages=($(setdiff "${apt_packages[*]}" "$installed_apt_packages"))
+
+if (( ${#apt_packages[@]} > 0 )); then
+  e_header "Installing APT packages (${#apt_packages[@]})"
+  for package in "${apt_packages[@]}"; do
+    e_arrow "$package"
+    [[ "$(type -t preinstall_$package)" == function ]] && preinstall_$package
+    sudo apt-get -qq install "$package" && \
+    [[ "$(type -t postinstall_$package)" == function ]] && postinstall_$package
+  done
+fi
+
+# Install debs via dpkg
+function __temp() { [[ ! -e "$1" ]]; }
+deb_installed_i=($(array_filter_i deb_installed __temp))
+
+if (( ${#deb_installed_i[@]} > 0 )); then
+  mkdir -p "$installers_path"
+  e_header "Installing debs (${#deb_installed_i[@]})"
+  for i in "${deb_installed_i[@]}"; do
+    e_arrow "${deb_installed[i]}"
+    deb="${deb_sources[i]}"
+    [[ "$(type -t "$deb")" == function ]] && deb="$($deb)"
+    installer_file="$installers_path/$(echo "$deb" | sed 's#.*/##')"
+    wget -O "$installer_file" "$deb"
+    sudo dpkg -i "$installer_file"
+  done
+fi
+
+# install bins from zip file
+function install_from_zip() {
+  local name=$1 url=$2 bins b zip tmp
+  shift 2; bins=("$@"); [[ "${#bins[@]}" == 0 ]] && bins=($name)
+  if [[ ! "$(which $name)" ]]; then
+    mkdir -p "$installers_path"
+    e_header "Installing $name"
+    zip="$installers_path/$(echo "$url" | sed 's#.*/##')"
+    wget -O "$zip" "$url"
+    tmp=$(mktemp -d)
+    unzip "$zip" -d "$tmp"
+    for b in "${bins[@]}"; do
+      sudo cp "$tmp/$b" "/usr/local/bin/$(basename $b)"
+    done
+    rm -rf $tmp
+  fi
+}
+
+# Run anything else that may need to be run.
+type -t finally >/dev/null && finally
--- init/30_python_pip.sh
+++ init/30_python_pip.sh
@@ -1,24 +1,36 @@
 # Homebrew installs python2 pip as "pip2"
-for pip_cmd in pip2 pip FAIL; do [[ "$(which $pip_cmd)" ]] && break; done
+for pip2_cmd in pip2 pip FAIL; do [[ "$(which $pip2_cmd)" ]] && break; done
+for pip3_cmd in pip3 pip FAIL; do [[ "$(which $pip3_cmd)" ]] && break; done
 
 # Exit if pip is not installed.
-[[ $pip_cmd == FAIL ]] && e_error "Pip needs to be installed." && return 1
+[[ $pip_cmd == FAIL ]] && e_error "Python 2 & 3 pip needs to be installed." && return 1
 
 # Add pip packages
-pip_packages=(
-  netifaces
-  powerline-status
+pip2_packages=(
   psutil
   tmuxp
 )
 
-installed_pip_packages="$($pip_cmd list 2>/dev/null | awk '{print $1}')"
-pip_packages=($(setdiff "${pip_packages[*]}" "$installed_pip_packages"))
+pip3_packages=(
+)
 
-if (( ${#pip_packages[@]} > 0 )); then
-  e_header "Installing pip packages (${#pip_packages[@]})"
-  for package in "${pip_packages[@]}"; do
+installed_pip2_packages="$($pip2_cmd list 2>/dev/null | awk '{print $1}')"
+installed_pip3_packages="$($pip3_cmd list 2>/dev/null | awk '{print $1}')"
+pip2_packages=($(setdiff "${pip2_packages[*]}" "$installed_pip2_packages"))
+pip3_packages=($(setdiff "${pip3_packages[*]}" "$installed_pip3_packages"))
+
+if (( ${#pip2_packages[@]} > 0 )); then
+  e_header "Installing pip2 packages (${#pip2_packages[@]})"
+  for package in "${pip2_packages[@]}"; do
     e_arrow "$package"
-    $pip_cmd install "$package"
+    $pip2_cmd install "$package"
+  done
+fi
+
+if (( ${#pip3_packages[@]} > 0 )); then
+  e_header "Installing pip3 packages (${#pip3_packages[@]})"
+  for package in "${pip3_packages[@]}"; do
+    e_arrow "$package"
+    $pip3_cmd install "$package"
   done
 fi
