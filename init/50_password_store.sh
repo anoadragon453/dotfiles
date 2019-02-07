954fc5e7893fcdd61638cd026425f0e04bb5be47
Andrew Morgan <andrew@amorgan.xyz>
2019-02-07 00:06:01 +0000

    Prevent password-store from installing twice

--- init/50_password_store.sh
+++ init/50_password_store.sh
@@ -1,10 +1,15 @@
-# Clone passwordstore passwords.
-# This assumes necessary ssh and gpg files are already in place
+# Clone password-store passwords.
 # TODO: Make more applicable to people installing from github (since we
 # currently just ask for a gitlab oauth key...)
 
 # TODO: Only for Linux for now. Need to get pass installed on osx
 is_debian || is_ubuntu || return 1
+
+# Stop if password-store is already installed
+if [ -d $HOME/.password-store ]
+then
+  return 1
+fi
 
 e_header "Setting up passwordstore... Ensure your GPG keys are set up."
 read -p "Enter your GitLab Oauth key: " key; echo
