93bc931e3122c2928c9f95fcc1f02a07897834eb
Andrew Morgan <andrew@amorgan.xyz>
2022-12-15 20:12:58 +0000

    Format code in shell.nix

--- shell.nix
+++ shell.nix
@@ -8,12 +8,10 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    alias foo="echo foo"
+    echo "You can apply this flake to your system with nixos-rebuild switch --flake .#"
 
-      echo "You can apply this flake to your system with nixos-rebuild switch --flake .#"
-
-      PATH=${pkgs.writeShellScriptBin "nix" ''
-        ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
-      ''}/bin:$PATH
-    '';
+    PATH=${pkgs.writeShellScriptBin "nix" ''
+      ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+    ''}/bin:$PATH
+  '';
   }
