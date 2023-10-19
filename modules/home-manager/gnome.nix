a6b8a2854b9f75a91dfa358e3fb3c2e8df716c88
Andrew Morgan <andrew@amorgan.xyz>
2023-10-19 23:27:52 +0100

    Further GNOME customisations via home-manager

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   # Set dconf settings.
   #
@@ -9,6 +9,47 @@
     #   * caps:none -> Disable caps lock.
     "org/gnome/desktop/input-sources" = {
       xkb-options = [ "caps:none" ];
+    };
+
+    # Enable user extensions.
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+    };
+
+    # Snap windows to edges of display.
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+    };
+
+    # Use the dark theme.
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
+    # Turn off lock-screen notifications.
+    # These will wake up the monitor when the computer is asleep,
+    # which wastes power.
+    "org/gnome/desktop/notifications" = {
+      show-in-lock-screen = false;
+    };
+
+    # Set the screen blank timeout to 15 minutes.
+    "org/gnome/desktop/session" = {
+      idle-delay = lib.hm.gvariant.mkUint32 900;
+    };
+
+    # Don't automatically suspend the computer.
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+
+    # Keybindings
+    "org/gnome/desktop/wm/keybindings" = {
+      # Close windows via Alt-Shift-Q.
+      close = [ "<Shift><Alt>q" ];
+
+      # Take a screenshot with Alt-Shift-S.
+      show-screenshot-ui = [ "<Shift><Alt>s" ];
     };
   };
 }
