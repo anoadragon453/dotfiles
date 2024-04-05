ed51b2992743b1b0f65042782a98679a9e7a2abb
Andrew Morgan <andrew@amorgan.xyz>
2024-04-05 07:25:54 +0100

    Add more git shell aliases

--- modules/base/git.nix
+++ modules/base/git.nix
@@ -11,11 +11,19 @@
     ];
 
     environment.shellAliases = {
-      gp = "git push";
-      gpp = "git pull";
-      gm = "git commit --amend";
+      ga = "git add";
       gc = "git commit";
+      gch = "git checkout";
+      gl = "git log";
+      gca = "git commit --amend";
+      gcm = "git commit -m";
+      gcp = "git cherry-pick";
+      gp = "git push";
+      gpf = "git push -f";
+      gpp = "git pull";
       gr = "git rebase -i";
+      gs = "git stash";
+      gsp = "git stash pop";
     };
   };
 }
