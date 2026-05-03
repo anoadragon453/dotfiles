87f9cad647849891f6a27fde8bf118308aef4658
Andrew Morgan <andrew@amorgan.xyz>
2026-05-03 14:59:43 +0100

    Installing lots of apps

--- modules/desktop/applications.nix
+++ modules/desktop/applications.nix
@@ -28,21 +28,24 @@ in
       discord
       mpv
       filezilla
+      parsec-bin
       qbittorrent
       signal-desktop
       telegram-desktop
-      tor-browser-bundle-bin
-      # thunderbird is installed via thunderbird.nix
+      tor-browser
+      thunderbird
 
       # Office
       obsidian
-      onlyoffice-bin
-      prusa-slicer
+      onlyoffice-desktopeditors
+      tectonic
+      gnome-text-editor # for quick note-taking, useful as it restores unsaved documents
 
       # Games
       prismlauncher
 
       # Media
+      audacity
       inkscape
       kdePackages.kdenlive
       krita
@@ -51,24 +54,37 @@ in
       obs-studio
       pavucontrol
       qpwgraph
-      stremio
-      (if (
-        # blender-hip sets up AMD HIP rendering for Blender.
-        # Only install blender-hip if we have an amd card.
-        cfg.hardware.graphics.primaryGPU == "amd" || cfg.hardware.graphics.extraGPU == "amd"
-      ) then blender-hip else blender)
+      (
+        if
+          (
+            # blender-hip sets up AMD HIP rendering for Blender.
+            # Only install blender-hip if we have an amd card.
+            cfg.hardware.graphics.primaryGPU == "amd" || cfg.hardware.graphics.extraGPU == "amd"
+          )
+        then
+          blender-hip
+        else
+          blender
+      )
 
       # Tools
+      android-studio
       appimage-run
       ascii
+      pkgsUnstable.codex
       contrast
       freecad
+      git-absorb
+      gnome-disk-utility
       nh
-      poppler_utils  # includes pdfunite
+      notify-desktop # for having codex-cli send desktop notifications
+      poppler-utils # includes pdfunite
       solaar
       translatelocally
       virt-manager
+      vscode
       wineWowPackages.stableFull
+      pkgsUnstable.zizmor
     ];
   };
 }
