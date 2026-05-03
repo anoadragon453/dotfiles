b07cb3353afaefb82f77ee105b0151f5810f879e
Andrew Morgan <andrew@amorgan.xyz>
2026-05-03 13:36:09 +0100

    Update KDE with new option names

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -14,26 +14,31 @@ in {
 
 
   config = mkIf (desktopMode && elem "kde" desktopGuiTypes) {
-    # Install and configure KDE Plasma5
-    services.xserver.desktopManager.plasma5.enable = true;
-
-    # Enable HiDPI scaling in Qt applications.
-    services.xserver.desktopManager.plasma5.useQtScaling = true;
+    # Install and configure KDE Plasma
+    services.desktopManager.plasma6.enable = true;
 
     # Fix GTK themes not applying in Wayland applications.
     # From https://nixos.wiki/wiki/KDE
     programs.dconf.enable = true;
 
     # Install some helpful packages when using KDE
-    # TODO:
+    sys.software = with pkgs.kdePackages; [
+      kcalc
+    ];
+    
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+    };
 
     # ksshaskpass conflicts with SeaHorse's askpass implementation when GNOME and KDE are both enabled.
     programs.ssh.askPassword = mkIf (elem "gnome" desktopGuiTypes) (mkForce "${pkgs.libsForQt5.ksshaskpass}");
 
     # Exclude some default KDE apps from being installed
     # Full list: https://github.com/NixOS/nixpkgs/blob/release-22.05/nixos/modules/services/x11/desktop-managers/gnome.nix#L483
-    # services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
-    # ];
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      elisa
+    ];
 
   };
 
