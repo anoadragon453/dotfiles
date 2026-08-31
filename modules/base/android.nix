a9ab2ad84b15ab02b07f5dc90da0a4a918a3199c
Andrew Morgan <andrew@amorgan.xyz>
2026-08-31 16:28:44 +0100

    Remove deprecated `programs.adb.enable` opt

--- modules/base/android.nix
+++ modules/base/android.nix
@@ -5,9 +5,6 @@ let
     cfg = config.sys;
 in {
   config = {
-    # Enable android device bridge support
-    programs.adb.enable = true;
-
     sys.software = [
       # adb, fastboot and other android cli tools
       android-tools
