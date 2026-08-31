b23cd8fe11be65e6369e80857aa5194e6add2cb9
Andrew Morgan <andrew@amorgan.xyz>
2026-08-31 16:22:36 +0100

    Fix cargo config, ensure it uses a unified target directory
    Plus enable garbage collection to reduce disk usage.

--- modules/home-manager/cargo.nix
+++ modules/home-manager/cargo.nix
@@ -2,13 +2,23 @@
 {
   # Consolidate all rust build files into a single directory on disk, to prevent
   # duplicate built dependencies across different projects.
-  xdg.configFile = { 
-    "cargo/config.toml" = {
-      enable = true;
-      text = ''
-        [build]
-        target-dir = "${config.xdg.configHome}/.cargo/target"
-      '';
+  programs.cargo = {
+    enable = true;
+    # Let devenv install cargo instead.
+    package = null;
+    settings = { 
+      build = {
+        target-dir = "${config.home.homeDirectory}/.cargo/target";
+      };
+      unstable = {
+        unstable-options = true;
+
+        # Unify all feature flags across the workspace.
+        feature-unification = true;
+
+        # Enable garbage collection of the target directory.
+        gc = true;
+      };
     };
   };
 }
